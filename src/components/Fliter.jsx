1abdceedc4b37597d94241954b61a64bb274e8c9
kimsoonil <37436918+kimsoonil@users.noreply.github.com>
2022-08-18 14:33:47 +0900

    fix filter

--- src/components/Fliter.jsx
+++ src/components/Fliter.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import "../assets/css/components.css";
-import { DateRangePicker } from "rsuite";
-import * as dayjs from "dayjs";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../assets/css/components.css';
+import * as dayjs from 'dayjs';
+import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@material-ui/pickers/adapter/date-fns';
+import { DateRangePicker, LocalizationProvider, DateRangeDelimiter } from '@material-ui/pickers';
 
 export const Fliter = ({}) => {
-  const [sort, setSort] = useState("new"); //popularity
-  const [date, setDate] = useState("All"); // All 3month Select
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const [keyword, setKeyword] = useState("");
-  const [startDateShow, setStartDateShow] = useState(false);
-  const [endDateShow, setEndDateShow] = useState(false);
+  const [sort, setSort] = useState('new'); //popularity
+  const [date, setDate] = useState('All'); // All 3month Select
+
+  const [keyword, setKeyword] = useState('');
+
+  const [selectedDate, handleDateChange] = React.useState([null, null]);
 
   const startDateChange = (value) => {
     setStartDate(value);
@@ -38,20 +39,14 @@ export const Fliter = ({}) => {
           <div className="filter-title">Sort by</div>
           <div className="flex-center sort-box">
             <div
-              className={
-                "box-left filter-box flex-center " +
-                (sort === "new" && "active")
-              }
-              onClick={() => setSort("new")}
+              className={'box-left filter-box flex-center ' + (sort === 'new' && 'active')}
+              onClick={() => setSort('new')}
             >
               최신순
             </div>
             <div
-              className={
-                "box-right filter-box flex-center " +
-                (sort === "popularity" && "active")
-              }
-              onClick={() => setSort("popularity")}
+              className={'box-right filter-box flex-center ' + (sort === 'popularity' && 'active')}
+              onClick={() => setSort('popularity')}
             >
               인기순
             </div>
@@ -62,69 +57,41 @@ export const Fliter = ({}) => {
             <div className="filter-title">Date</div>
             <div className="flex-center date-box">
               <div
-                className={
-                  "box-left filter-box flex-center " +
-                  (date === "All" && "active")
-                }
-                onClick={() => setDate("All")}
+                className={'box-left filter-box flex-center ' + (date === 'All' && 'active')}
+                onClick={() => setDate('All')}
               >
                 A week
               </div>
               <div
-                className={
-                  "filter-box flex-center " + (date === "3month" && "active")
-                }
-                onClick={() => setDate("3month")}
+                className={'filter-box flex-center ' + (date === '3month' && 'active')}
+                onClick={() => setDate('3month')}
               >
                 3 month
               </div>
               <div
-                className={
-                  "box-right filter-box flex-center " +
-                  (date === "select" && "active")
-                }
-                onClick={() => setDate("select")}
+                className={'box-right filter-box flex-center ' + (date === 'select' && 'active')}
+                onClick={() => setDate('select')}
               >
                 Select a Date
               </div>
             </div>
           </div>
-          <div className={date !== "select" ? "none" : "calendar flex-center"}>
-            <div className={"relative " + (startDateShow && "active")}>
-              <input
-                className="date-input"
-                value={dayjs(startDate).format("YYYY-MM-DD")}
-                onFocus={() => startDateFouce()}
+          <div className={date !== 'select' ? 'none' : 'calendar flex-center'}>
+            <LocalizationProvider dateAdapter={DateFnsUtils}>
+              <DateRangePicker
+                startText="Check-in"
+                endText="Check-out"
+                value={selectedDate}
+                onChange={(date) => handleDateChange(date)}
+                renderInput={(startProps, endProps) => (
+                  <>
+                    <TextField {...startProps} />
+                    <DateRangeDelimiter> to </DateRangeDelimiter>
+                    <TextField {...endProps} />
+                  </>
+                )}
               />
-              <div
-                className="ic_calendar"
-                onClick={() => startDateFouce()}
-              ></div>
-            </div>
-            <div className={"relative " + (endDateShow && "active")}>
-              <input
-                className="date-input"
-                value={dayjs(endDate).format("YYYY-MM-DD")}
-                onFocus={() => endDateFouce()}
-              />
-              <div
-                className={"ic_calendar"}
-                onClick={() => endDateFouce()}
-              ></div>
-            </div>
-
-            <Calendar
-              className={startDateShow ? "" : "hide"}
-              onChange={startDateChange}
-              locale="en-EN"
-              // selectRange={true}
-            />
-            <Calendar
-              className={endDateShow ? "" : "hide"}
-              onChange={endDateChange}
-              locale="en-EN"
-              // selectRange={true}
-            />
+            </LocalizationProvider>
           </div>
         </div>
 
@@ -138,10 +105,7 @@ export const Fliter = ({}) => {
               onChange={(e) => setKeyword(e.target.value)}
             />
             <div className="seachIc">
-              <img
-                src={require("../assets/components/ic_search_wh.svg").default}
-                alt=""
-              />
+              <img src={require('../assets/components/ic_search_wh.svg').default} alt="" />
             </div>
           </div>
         </div>
